9a64968fb0d169878a6d6cc2237fb22a3a7acd14
Tharindu <tharindubathigama@gmail.com>
2023-06-12 19:14:51 +0100

    add 'labels' table migration

--- app/models/label.py
+++ app/models/label.py
@@ -1,15 +1,22 @@
 from uuid import uuid4
 from datetime import datetime
 from app.config.database import Base
-from sqlalchemy import Column, String, DateTime
 from app.utils.guid import GUID
+from .dataset import Dataset
+from sqlalchemy import Column, String, DateTime, UniqueConstraint, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Dataset(Base):
-    __tablename__ = "datasets"
+class Label(Base):
+    __tablename__ = "labels"
 
     id = Column(GUID(), primary_key=True, index=True, default=uuid4)
     name = Column(String, nullable=False)
+    dataset_id = Column(GUID(), ForeignKey(Dataset.id))
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
     updated_at = Column(DateTime, nullable=False,
                         default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    dataset = relationship('Dataset', foreign_keys='Label.dataset_id')
+
+    __table_args__ = (UniqueConstraint('dataset_id', 'name'),)
