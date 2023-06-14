990aa3892aebe33ee0f818fd3e4c925ab481f30f
Tharindu <tharindubathigama@gmail.com>
2023-06-14 19:30:00 +0100

    workflow update

--- run-test.sh
+++ run-test.sh
@@ -5,5 +5,5 @@ DATABASE_URL=sqlite:///$DB_PATH
 
 rm -f $DB_PATH
 
-DATABASE_URL=$DATABASE_URL alembic upgrade head
-DATABASE_URL=$DATABASE_URL pytest
+DATABASE_URL=$DATABASE_URL poetry run alembic upgrade head
+DATABASE_URL=$DATABASE_URL poetry run pytest
