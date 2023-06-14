9335462dd49a4495cf30ed851ff812366fbb24ae
Tharindu <tharindubathigama@gmail.com>
2023-06-14 18:50:22 +0100

    refactored migrations files

--- migrations/env.py
+++ migrations/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from app.config.database import DATABASE_URL, get_metadata
-from app.models import *
+from app.models import *  # noqa: F403, F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -69,9 +69,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
