c007e8c958e3bc38807e16dfbf0b1929c55fead9
Tharindu <tharindubathigama@gmail.com>
2023-06-17 12:29:15 +0100

    update test script

--- run-test.sh
+++ run-test.sh
@@ -5,5 +5,6 @@ DATABASE_URL=sqlite:///$DB_PATH
 
 rm -f $DB_PATH
 
+(cd ./frontend && pnpm install && pnpm run build)
 DATABASE_URL=$DATABASE_URL poetry run alembic upgrade head
 DATABASE_URL=$DATABASE_URL poetry run pytest
