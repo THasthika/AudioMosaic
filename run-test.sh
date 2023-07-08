948ccab11dec801e872df105cff4e7ed34faee73
Tharindu <tharindubathigama@gmail.com>
2023-07-08 17:19:38 +0100

    test runner modified

--- run-test.sh
+++ run-test.sh
@@ -7,7 +7,7 @@ APP_DIST_PATH=./frontend
 rm -f $DB_PATH
 
 DATABASE_URL=$DATABASE_URL poetry run alembic upgrade head
-APP_DIST_PATH=$APP_DIST_PATH DATABASE_URL=$DATABASE_URL poetry run pytest app/tests_e2e -v -s
+APP_DIST_PATH=$APP_DIST_PATH DATABASE_URL=$DATABASE_URL poetry run pytest app/tests_e2e
 
 # clear storage created
 rm -r storage/audio_samples/*
